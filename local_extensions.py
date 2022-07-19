5cdadb276e8195f8775afa856a9555980a173ebf
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-07-19 16:04:21 +0200

    Automatically fill the current year

--- local_extensions.py
+++ local_extensions.py
@@ -1,5 +1,18 @@
+import datetime
+
 from jinja2.ext import Extension
 
+#
+# Export the current date
+#
+
+class CurrentDateExtension(Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+
+        environment.globals.update({
+            "current_year": datetime.datetime.utcnow().year
+        })
 
 #
 # Check whether we have pre-commit or not and make the result available
